2e89101a568794ff3b2569f7aad67229e1febb62
sihun <thrchtlgns@naver.com>
2022-06-26 14:38:55 +0900

    [comment] 내용수정

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          시훈
         </p>
         <a
           className="App-link"
